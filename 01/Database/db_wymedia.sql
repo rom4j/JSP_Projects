b1b83085aaea4a46046c476170e93a4becfbcee8
Rom <292583883@qq.com>
2015-06-18 08:00:37 +0800

    init01

---
+++ 01/Database/db_wymedia.sql
@@ -0,0 +1,139 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.0.51b-community-nt-log : Database - db_wymedia
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_wymedia` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `db_wymedia`;
+
+/*Table structure for table `dtproperties` */
+
+DROP TABLE IF EXISTS `dtproperties`;
+
+CREATE TABLE `dtproperties` (
+  `id` int(11) NOT NULL auto_increment,
+  `objectid` int(11) default NULL,
+  `property` varchar(64) collate utf8_unicode_ci default NULL,
+  `value` varchar(255) collate utf8_unicode_ci default NULL,
+  `uvalue` varchar(510) collate utf8_unicode_ci default NULL,
+  `lvalue` text collate utf8_unicode_ci,
+  `version` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `dtproperties` */
+
+/*Table structure for table `tb_admin` */
+
+DROP TABLE IF EXISTS `tb_admin`;
+
+CREATE TABLE `tb_admin` (
+  `id` int(11) NOT NULL auto_increment,
+  `account` varchar(50) collate utf8_unicode_ci default NULL,
+  `password` varchar(50) collate utf8_unicode_ci default NULL,
+  `visit` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tb_admin` */
+
+/*Table structure for table `tb_info` */
+
+DROP TABLE IF EXISTS `tb_info`;
+
+CREATE TABLE `tb_info` (
+  `id` int(11) NOT NULL auto_increment COMMENT 'ID自动编号',
+  `info_type` smallint(2) default NULL COMMENT '信息类别',
+  `info_title` varchar(80) collate utf8_unicode_ci default NULL COMMENT '信息标题',
+  `info_content` varchar(1000) collate utf8_unicode_ci default NULL COMMENT '信息内容',
+  `info_linkman` varchar(50) collate utf8_unicode_ci default NULL COMMENT '联系人',
+  `info_phone` varchar(50) collate utf8_unicode_ci default NULL COMMENT '联系电话',
+  `info_email` varchar(100) collate utf8_unicode_ci default NULL COMMENT 'E-mail地址',
+  `info_date` datetime default NULL COMMENT '发布时间',
+  `info_state` varchar(1) collate utf8_unicode_ci default '0' COMMENT '审核状态',
+  `info_payfor` varchar(1) collate utf8_unicode_ci default '0' COMMENT '付费状态',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tb_info` */
+
+insert  into `tb_info`(`id`,`info_type`,`info_title`,`info_content`,`info_linkman`,`info_phone`,`info_email`,`info_date`,`info_state`,`info_payfor`) values (1,1,'test1','testessdfgdsfsd','zs','18758917700','2@qq.com','2015-06-18 00:10:51','0','1');
+
+/*Table structure for table `tb_media` */
+
+DROP TABLE IF EXISTS `tb_media`;
+
+CREATE TABLE `tb_media` (
+  `id` int(11) NOT NULL auto_increment,
+  `media_title` varchar(100) collate utf8_unicode_ci default NULL,
+  `media_type` varchar(100) collate utf8_unicode_ci default NULL,
+  `media_src` varchar(100) collate utf8_unicode_ci default NULL,
+  `media_pic` varchar(100) collate utf8_unicode_ci default NULL,
+  `media_info` varchar(400) collate utf8_unicode_ci default NULL,
+  `media_uptime` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `media_lookCount` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tb_media` */
+
+/*Table structure for table `tb_mediar` */
+
+DROP TABLE IF EXISTS `tb_mediar`;
+
+CREATE TABLE `tb_mediar` (
+  `id` int(11) NOT NULL auto_increment,
+  `mediaR_rootId` int(11) default NULL,
+  `mediaR_author` varchar(50) collate utf8_unicode_ci default NULL,
+  `mediaR_content` text collate utf8_unicode_ci,
+  `mediaR_time` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tb_mediar` */
+
+/*Table structure for table `tb_type` */
+
+DROP TABLE IF EXISTS `tb_type`;
+
+CREATE TABLE `tb_type` (
+  `id` int(11) NOT NULL auto_increment COMMENT 'ID自动编号',
+  `type_sign` int(11) default NULL COMMENT '类别标识',
+  `type_name` varchar(20) collate utf8_unicode_ci default NULL COMMENT '类别名称',
+  `type_intro` varchar(20) collate utf8_unicode_ci default NULL COMMENT '类别介绍',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tb_type` */
+
+insert  into `tb_type`(`id`,`type_sign`,`type_name`,`type_intro`) values (1,1,'二手物品','卖你的二手');
+
+/*Table structure for table `tb_user` */
+
+DROP TABLE IF EXISTS `tb_user`;
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_name` varchar(20) collate utf8_unicode_ci default NULL,
+  `user_password` varchar(10) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tb_user` */
+
+insert  into `tb_user`(`id`,`user_name`,`user_password`) values (1,'zs','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ 01/src/com/yxq/dao/DB.java
@@ -0,0 +1,136 @@
+package com.yxq.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * 数据库操作工具类
+ * @author le
+ * getCon()获取数据库连接,closed()关闭数据库连接,doPstm()增删改查操作
+ */
+public class DB {
+	private Connection con;
+	private PreparedStatement pstm;
+	//----数据库连接参数设置----
+	/**
+	 * 用户名
+	 */
+	private String user="root";
+	/**
+	 * 用户密码
+	 */
+	private String password="3171204";
+	/**
+	 *驱动类名
+	 *	mysql 		com.mysql.jdbc.Driver
+	 *	sqlserver 	com.microsoft.sqlserver.jdbc.SQLServerDriver
+	 *	oracle		oracle.jdbc.driver.OracleDriver
+	 */
+	private String className="com.mysql.jdbc.Driver";
+	/**
+	 * 数据库URL
+	 * mysql		jdbc:mysql://127.0.0.1:3306/db_wymedia?useUnicode=true&characterEncoding=UTF-8
+	 * sqlserver	jdbc:sqlserver://localhost:1433;DatabaseName=db_wymedia
+	 * oracle 		jdbc:oracle:thin:@localhost:1521:orcl
+	 */
+	private String url="jdbc:mysql://127.0.0.1:3306/db_wymedia?useUnicode=true&characterEncoding=UTF-8";
+	
+	/**
+	 * 构造器
+	 * 	类加载初始化
+	 */
+	public DB(){
+		try{
+			Class.forName(className);
+		}catch(ClassNotFoundException e){
+			System.out.println("加载数据库驱动失败！");
+			e.printStackTrace();
+		}
+	}
+
+	
+	/**
+	 * 创建数据库连接
+	 * 	获取数据连接
+	 * @return Connection
+	 */
+	public Connection getCon(){
+		try {
+			con=DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			System.out.println("创建数据库连接失败！");
+			con=null;
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	/**
+	 * 执行数据增删改查操作
+	 * @param sql
+	 * @param params
+	 */
+	public void doPstm(String sql,Object[] params){
+		if(sql!=null&&!sql.equals("")){
+			if(params==null)
+				params=new Object[0];
+			
+			getCon();
+			if(con!=null){
+				try{		
+					System.out.println("---->sql--"+sql);
+					pstm=con.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+					for(int i=0;i<params.length;i++){
+						System.out.println("---->"+i+"--"+params[i]);
+						pstm.setObject(i+1,params[i]);
+					}
+					pstm.execute();
+				}catch(SQLException e){
+					System.out.println("doPstm()方法出错！");
+					e.printStackTrace();
+				}				
+			}			
+		}
+	}
+	
+	/**
+	 * 获取结果集
+	 * @return
+	 * @throws SQLException
+	 */
+	public ResultSet getRs() throws SQLException{
+		return pstm.getResultSet();		
+	}
+	/**
+	 * Retrieves the current result as an update count; 
+	 * if the result is a ResultSet object or there are no more results, -1 is returned. This method should be called only once per result.
+	 * @return
+	 * @throws SQLException
+	 */
+	public int getCount() throws SQLException{
+		return pstm.getUpdateCount();		
+	}
+	/**
+	 * 关闭连接
+	 */
+	public void closed(){
+		try{
+			if(pstm!=null)
+				pstm.close();			
+		}catch(SQLException e){
+			System.out.println("关闭pstm对象失败！");
+			e.printStackTrace();
+		}
+		try{
+			if(con!=null){
+				con.close();
+			}
+		}catch(SQLException e){
+			System.out.println("关闭con对象失败！");
+			e.printStackTrace();
+		}
+	}
+}
